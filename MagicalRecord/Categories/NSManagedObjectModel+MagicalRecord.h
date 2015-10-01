203984d8886b01a18af083e2470f846fb7fff8d9
Jacky Wu <jacky.wu@udemy.com>
2015-10-01 01:32:28 -0700

    add NSManagedObjectModel+MagicalRecord.h and NSManagedObjectContext+MagicalObserving.m

---
+++ MagicalRecord/Categories/NSManagedObjectModel+MagicalRecord.h
@@ -0,0 +1,13 @@
+//
+//  NSManagedObjectModel+MagicalRecord.h
+//  MagicalRecord
+//
+//  Created by Jacky Wu on 10/1/15.
+//  Copyright © 2015 Magical Panda Software LLC. All rights reserved.
+//
+
+#ifndef NSManagedObjectModel_MagicalRecord_h
+#define NSManagedObjectModel_MagicalRecord_h
+
+
+#endif /* NSManagedObjectModel_MagicalRecord_h */
